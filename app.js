68bf2cd8ac521b39c034dc4339de66a1c783f751
SohilaAshat <sohila.mahmoud6060@gmail.com>
2026-04-07 11:46:27 +0200

    Add Tea to task list

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ async function connectDB() {
       { id: 2, name: 'Eggs', status: 'done' },
       { id: 3, name: 'Bread', status: 'pending' },
       { id: 4, name: 'Butter', status: 'pending' },
-      { id: 5, name: 'Orange juice', status: 'pending' }
+      { id: 5, name: 'Orange juice', status: 'pending' },
+      { id: 7, name: 'Tea', status: 'pending' }
+
     ]);
   }
 }
